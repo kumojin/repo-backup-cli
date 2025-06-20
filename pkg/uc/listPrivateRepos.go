819781c9d7b1034a383ac81f38141738c558c8bd
Leo Belanger <belanger.leo01@gmail.com>
2025-06-20 16:28:16 -0400

    feat: paginate list repos

--- pkg/uc/listPrivateRepos.go
+++ pkg/uc/listPrivateRepos.go
@@ -6,6 +6,8 @@ import (
 	"github.com/google/go-github/v72/github"
 )
 
+const maxPerPage = 100
+
 type ListPrivateReposUseCase interface {
 	Do(ctx context.Context, organization string) ([]github.Repository, error)
 }
@@ -21,16 +23,33 @@ func NewListPrivateReposUseCase(client *github.Client) ListPrivateReposUseCase {
 }
 
 func (uc *listPrivateReposUseCase) Do(ctx context.Context, organization string) ([]github.Repository, error) {
-	// TODO: implement pagination
-	repos, _, err := uc.githubClient.Repositories.ListByOrg(
-		ctx,
-		organization,
-		&github.RepositoryListByOrgOptions{
-			Type: "private",
-		},
-	)
-	if err != nil {
-		return nil, err
+	var repos []*github.Repository
+	hasMore := true
+	page := 1
+
+	for hasMore {
+		pageRepos, res, err := uc.githubClient.Repositories.ListByOrg(
+			ctx,
+			organization,
+			&github.RepositoryListByOrgOptions{
+				Type: "private",
+				ListOptions: github.ListOptions{
+					PerPage: maxPerPage,
+					Page:    page,
+				},
+			},
+		)
+		if err != nil {
+			return nil, err
+		}
+
+		repos = append(repos, pageRepos...)
+
+		if res.NextPage > page {
+			page = res.NextPage
+		} else {
+			hasMore = false
+		}
 	}
 
 	var filteredRepos []github.Repository
