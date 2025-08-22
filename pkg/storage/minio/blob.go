3c88d775cdb53d0505a8d0b1bbf4c6c176ab0585
Leo Belanger <belanger.leo01@gmail.com>
2025-08-22 15:28:47 -0400

    refactor: simplify minio blob client

--- pkg/storage/minio/blob.go
+++ pkg/storage/minio/blob.go
@@ -2,10 +2,8 @@ package minio
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"io"
-	"net/url"
 
 	"github.com/kumojin/repo-backup-cli/pkg/config"
 	"github.com/kumojin/repo-backup-cli/pkg/storage"
@@ -22,51 +20,12 @@ func NewBlobRepository(cfg *config.Config, client *minio.Client) storage.BlobRep
 }
 
 func (r defaultBlobRepository) Upload(ctx context.Context, blobName string, in io.Reader) (string, error) {
-	size, err := getSize(in)
-	if err != nil {
-		return "", fmt.Errorf("failed to get size of input stream: %w", err)
-	}
-
-	_, err = r.client.PutObject(ctx, r.cfg.ObjectStorageConfig.BucketName, blobName, in, size, minio.PutObjectOptions{
+	info, err := r.client.PutObject(ctx, r.cfg.ObjectStorageConfig.BucketName, blobName, in, -1, minio.PutObjectOptions{
 		ContentType: "application/octet-stream",
 	})
 	if err != nil {
 		return "", fmt.Errorf("failed to upload object to object storage: %w", err)
 	}
 
-	return r.getUrl(blobName)
-}
-
-func (r defaultBlobRepository) getUrl(blobName string) (string, error) {
-	scheme := "http"
-	if r.cfg.ObjectStorageConfig.UseSSL {
-		scheme = "https"
-	}
-
-	baseURL := fmt.Sprintf("%s://%s", scheme, r.cfg.ObjectStorageConfig.Endpoint)
-	url, err := url.JoinPath(baseURL, r.cfg.ObjectStorageConfig.BucketName, blobName)
-	if err != nil {
-		return "", fmt.Errorf("failed to construct MinIO URL: %w", err)
-	}
-
-	return url, nil
-}
-
-func getSize(stream io.Reader) (int64, error) {
-	seeker, ok := stream.(io.Seeker)
-	if !ok {
-		return 0, errors.New("cannot cast reader to seeker")
-	}
-
-	size, err := seeker.Seek(0, io.SeekEnd)
-	if err != nil {
-		return 0, err
-	}
-
-	_, err = seeker.Seek(0, io.SeekStart)
-	if err != nil {
-		return 0, err
-	}
-
-	return size, nil
+	return info.Location, nil
 }
